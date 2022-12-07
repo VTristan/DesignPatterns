dbc5468502538c66559bce0a276daf7566bb94a1
Tristan <t.vlasic-ext@lefebvre-sarrut.eu>
2022-12-07 23:20:28 +0100

    add comment for decorator pattern

--- src/main/java/pattern/structural/decorator/Main.java
+++ src/main/java/pattern/structural/decorator/Main.java
@@ -3,18 +3,29 @@ package pattern.structural.decorator;
 public class Main {
 
     public static void main(String[] args) {
-        AbstractComponent component = new ComponentImpl();
+        AbstractComponent component;
+        /* Nous créons un objet ComponentImpl qui implémente l'interface AbstractComponent */
+        component = new ComponentImpl();
+        /* Puis nous appelons sa méthode operation() */
         component.operation();
 
+        System.out.print("----------");
+        /* Imaginons que nous souhaitons ajouter un nouveau traitement avant et après l'appel de la méthode operation(),
+        dans ce cas, nous pouvons créer un décorateur */
         System.out.println("----------");
-        //Decorator 1
+
+        /* Le Decorator n°1 va se charger d'ajouter les traitements avant/après,
+        pour cela nous devons affecter à la variable 'component' l'objet DecoratorImpl qui prend en paramètre cette même variable */
         component = new DecoratorImpl(component);
+        /* Il faut continuer à appeler les méthodes déclarées dans l'interface AbstractComponent */
         component.operation();
+
+        System.out.print("----------");
+        /* Nous pouvons créer autant de décorateur que voulu */
         System.out.println("----------");
 
-        //Decorator 2
+        /* Decorator n°2 */
         component = new DecoratorImpl2(component);
         component.operation();
     }
-
 }
